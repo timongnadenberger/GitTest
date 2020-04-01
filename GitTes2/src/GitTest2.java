58918b3bff5874deb17891eb1ba01c70a712b5ce
Timon Gnadenberger <t.gnadenberger@gmail.com>
2020-04-01 11:33:40 +0200

    alles neu

--- GitTes2/src/GitTest2.java
+++ GitTes2/src/GitTest2.java
@@ -4,6 +4,7 @@ public class GitTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ich teste Git nochmal");
+		System.out.println("alles neu");
 	}
 
 }
